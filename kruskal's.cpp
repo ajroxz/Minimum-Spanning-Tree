6b88bc8e83072a8066ad97de91a4af01b06d1938
ajroxz <45990422+ajroxz@users.noreply.github.com>
2020-10-02 16:17:35 +0530

    first commit

---
+++ kruskal's.cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h>
+#define v 9
+using namespace std;
+int arr[v];
+
+bool cmp(pair<int,int> x,pair<int,int> y){
+
+    
+
+    return x.second<y.second;
+    
+}
+
+int find(int x){
+
+    if(arr[x]==-1)
+        return x;
+    
+    return find(arr[x]);
+}
+
+bool mkmst(int x,int y)
+{
+    /* for(auto a:arr)
+    {
+        cout<<a<<endl;
+    }
+ */
+   
+
+    int a,b;
+    
+    a=find(x);
+    b=find(y);
+
+    if(a==x&&b==y){
+        arr[x]=y;
+        return true;
+    }
+    else if(a!=b){
+        
+
+        arr[a]=b;
+        
+        return true;
+    }
+
+    
+
+    
+    return false;
+
+    
+}
+
+void printmst(vector<pair<int,int>> lst)
+{
+    cout<<"size = "<<lst.size()<<endl;
+    vector<pair<int,int>>::iterator itr;
+    cout<<"EDGES"<<"\t"<<"WEIGHT"<<endl;
+    int i,j;
+    
+    for(itr=lst.begin();itr!=lst.end();itr++)
+    {
+        
+        
+
+        if(itr->first%10==itr->first)
+        {
+            i=0;
+            j=itr->first;
+            if(mkmst(i,j)){
+                cout<<i<<" - "<<j<<"\t"<<itr->second<<endl;
+            }
+            else{
+                continue;
+            }
+            
+
+        }
+        else
+        {
+            
+            i=itr->first/10;
+            j=itr->first%10; 
+            if(mkmst(i,j)){
+                cout<<i<<" - "<<j<<"\t"<<itr->second<<endl;
+
+            }
+            else{
+                continue;
+            }
+            
+            
+        }
+        
+        
+        
+    }
+}
+
+
+
+void krus(int g[v][v])
+{
+    vector<pair<int,int>> set;
+    map<int,int> lst;
+    
+    for(int i=0;i<v;i++)
+    {
+        for(int j=i+1;j<v;j++)
+        {
+            if(g[i][j])
+            {
+                
+                lst.insert({10*i+j,g[i][j]});
+                
+            }
+        }
+       
+    }
+    
+
+    for(auto a:lst)
+    {
+        set.push_back(a);
+    }
+    
+    sort(set.begin(),set.end(),cmp);
+    
+    printmst(set);
+
+    
+}
+
+
+
+int main()
+{
+    int graph[v][v] = { {0,4,0,0,0,0,0,8,0},
+                        {4,0,8,0,0,0,0,11,0},
+                        {0,8,0,7,0,4,0,0,2},
+                        {0,0,7,0,9,14,0,0,0},
+                        {0,0,0,9,0,10,0,0,0},
+                        {0,0,4,14,10,0,2,0,0},
+                        {0,0,0,0,0,2,0,1,6},
+                        {8,11,0,0,0,0,1,0,7},
+                        {0,0,2,0,0,0,6,7,0} };
+    memset(arr,-1,sizeof(int)*v);
+    krus(graph);
+
+    return 0;
+}
---
+++ prim's.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define v 5
+
+
+
+int min_index(int g[v][v],bool mstset[v],int dist[v])// this function is to return the node which has min distance from parent
+{
+	int min = INT_MAX,index;
+
+	for(int i=0;i<v;i++)
+	{
+		if(mstset[i]==false&&dist[i]<min)
+		{
+			min = dist[i];
+			index =i;
+		}
+	}
+
+	return index;
+
+}
+
+void printmst(int dist[], int parent[])
+{
+	cout<<"Edge"<<"\t"<<"weight"<<endl;
+	for(int counter=1;counter<v;counter++)
+	{	
+		cout<<parent[counter]<<" - "<<counter<<"\t"<<dist[counter]<<endl;
+	}
+}
+
+
+void prims(int g[v][v]){
+
+	int dist[v];
+	int parent[v];
+
+	bool mstset[v];
+
+	for(int i=0;i<v;i++)
+	{
+		mstset[i]=false;
+		dist[i]=INT_MAX;
+	}
+
+	dist[0]=0;
+	parent[0]=-1;//root node
+
+	for(int i=0;i<v;i++)
+	{
+	
+		int u = min_index(g,mstset,dist);
+
+		mstset[u]=true;
+
+		for(int j=0;j<v;j++)//loop to update the dist of adjacent nodes of selected node
+		{
+			if(g[u][j]&&mstset[j]==false&&g[u][j]<dist[j])
+			{
+				dist[j]=g[u][j];
+				parent[j]=u;
+			}
+		}
+
+
+	}
+
+	printmst(dist,parent);
+
+}
+
+
+
+
+
+
+int main()
+{
+	int graph[v][v] = { { 0, 2, 0, 6, 0 }, 
+                        { 2, 0, 3, 8, 5 }, 
+                        { 0, 3, 0, 0, 7 }, 
+                        { 6, 8, 0, 0, 9 }, 
+                        { 0, 5, 7, 9, 0 } };
+	prims(graph);
+	return 0;
+}
